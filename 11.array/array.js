61dfd489b79d79f5cf38987b57eaf3cf1220c3ba
Ahmajist <ahmajist@gmail.com>
2024-10-27 19:16:25 +0700

    [menambahkan beberapa folder]

---
+++ 11.array/array.js
@@ -0,0 +1,17 @@
+// const names = [
+//      "Theo", "Naruto", "Doraemon"
+// ];
+
+// // names.push("Ahmaji");// method utk menambahkan element dengan index terakhir/paling akhir dari array kedalam array
+// // names.splice(2, 1) // method utk menghapus element pada array yang membutuhkan parameter (parameter 1 untuk memulai penghapusan. sedangkan parameter kedua utk menghapus jumlah element yang ingin kita hapus)
+// // names.reverse() //method utk membalikan urutan isi/element di dalam array
+// names[0] = "Nobita" // menimpa element dengan index ke 0 (isi di dalam array names) bisa tipe data apa saja
+// console.log(names)// untuk menampilkan semua index di dalam array
+// console.log(names.length) //utk menampilkan panjang index di dalam array
+
+const grid = [
+     [1, 2, 3], //index 0
+     [4, 5, 6], //index 1
+     [7, 8, 9], //index 2
+];
+console.log(grid[0][1])
